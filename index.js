283f24113c92f7ad3e05b3ea660af53961d0b428
Juan Fuentes <59918928+JuanFuentes20@users.noreply.github.com>
2023-09-02 02:37:12 +0300

    Finalized solution

---
+++ index.js
@@ -0,0 +1,46 @@
+import jsonData from './data.json' assert { type: 'json' };
+
+const categories = document.querySelector('.categories')
+console.log(jsonData)
+
+jsonData.forEach(category => {
+    const listElement = document.createElement('li')
+    const listDiv = document.createElement('div')
+    listDiv.classList.add('category')
+
+    const iconAndCategoryName = document.createElement('div')
+
+    const icon = document.createElement('img')
+    icon.src = category.icon
+    icon.alt = "Icon"
+    icon.classList.add('icon')
+
+    iconAndCategoryName.appendChild(icon)
+
+    const score = document.createElement('div')
+    score.style.display = 'flex'
+    score.style.flexDirection = 'row'
+
+
+    const categoryName = document.createElement('p')
+    categoryName.innerText = category.category
+    iconAndCategoryName.appendChild(categoryName)
+    const categoryScore = document.createElement('p')
+    categoryScore.innerText = category.score
+    categoryScore.style.fontWeight = 600
+    categoryScore.style.color = 'black'
+    const totalScore = document.createElement('p')
+    totalScore.innerText = '/ 100'
+    totalScore.style.fontWeight = 400
+    totalScore.style.color = '#D3D3D3'
+
+    score.appendChild(categoryScore)
+    score.appendChild(totalScore)
+    listDiv.appendChild(iconAndCategoryName)
+    listDiv.appendChild(score)
+    listElement.appendChild(listDiv)
+    
+    
+    
+    categories.appendChild(listElement)
+})
